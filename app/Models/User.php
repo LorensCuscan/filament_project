97e6fca38b3ada942a796c9718497180016b1127
LorensCuscan <lorenscuscan@gmail.com>
2023-01-21 15:44:51 -0300

    feat: added super user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,27 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use BezhanSalleh\FilamentShield\Traits\HasFilamentShield;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+
+
+
+
+
+
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+
+
+    use HasApiTokens, HasFactory, Notifiable, HasFilamentShield;
+
+    
 
     /**
      * The attributes that are mass assignable.
@@ -21,10 +34,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        
     ];
-    public function user_id(){
-        return $this->hasMany(User::class);
-    }
+    
+
+  
 
     /**
      * The attributes that should be hidden for serialization.
